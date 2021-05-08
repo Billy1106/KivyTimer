28ef3aa4dcf5cb57b672547192164af08c559a50
Billy <ryotabilly@gmail.com>
2021-05-08 14:54:55 -0700

    Implement Simple Timer

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # KivyTimer
-Timer
+
+
+## Aim
+
+### Phase 1
+* Simple timer that you can set/start timer (hr:min:sec)
+* Keep showing deadlines foremost
+
+### Phase 2
+* Visually able to set the deadline of todo (e.g assignments)
+* Can switch (hr:min:sec) to (day:hr:min)
+
+### Phase 3
+* Can set several timers
+* Can save/load timers
+
+## Functions
+
+### Set Timer
+
+* Setting timer (00:00:00) 
+
+### Reset Timer
+
+* Resetting timer
+
+### Start Timer
+
+* Start timer if its already set
+* Count down until 0 then set back to the initial time
+
+### Stop Timer
+
+* Temporally stop the timer
+* Continue Timer if start is pressed
+* Restart then set back to the initial timer 
+
---
+++ SimpleTimer.py
@@ -0,0 +1,83 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+from kivy.properties import Property
+from kivy.properties import BooleanProperty
+from kivy.uix.label import Label
+
+from kivy.core.text import LabelBase,DEFAULT_FONT
+from kivy.resources import resource_add_path
+from kivy.clock import Clock
+from kivy.config import Config
+Config.set('graphics','width','500')
+Config.set('graphics','height','350')
+
+
+class TimerWidget(Widget):
+    
+    count = StringProperty()
+    count_hr = StringProperty()
+    count_min = StringProperty()
+    count_sec = StringProperty()
+    is_counting = BooleanProperty(False)
+    init_count = 0
+    
+    def __init__(self,**kwargs):
+        self.update_time(0)
+        super(TimerWidget,self).__init__(**kwargs)
+
+    def start_countdown(self):
+        if(not self.is_counting and int(self.convert_to_sec()) > 0):
+            self.is_counting = True
+            self.init_count = self.convert_to_sec()
+            Clock.schedule_interval(self.countdown,1)
+
+    def set_countdown(self,mod):
+        if(not self.is_counting):
+            self.update_time(self.convert_to_sec()+int(mod))
+
+    def stop_countdown(self):
+        if(self.is_counting):
+            self.is_counting = False
+            Clock.unschedule(self.countdown)
+
+    def reset_countdown(self):
+        if(self.is_counting):
+            self.update_time(self.init_count)
+        else:
+            self.update_time(0)
+        self.stop_countdown()
+
+    def countdown(self,dt):#Require two arg
+        current_time = str(self.convert_to_sec()-1)
+        if(int(current_time)<=0):
+            self.update_time(0)
+            self.stop_countdown()
+        self.update_time(current_time)
+
+    def convert_to_sec(self):
+        return int(self.count_hr)*3600+int(self.count_min)*60+int(self.count_sec)
+    
+    def update_time(self,num):
+        num = int(num)
+        if(num<0):
+            return False
+        hr = str(int(num/3600))
+        min = str(int((num%3600)/60))
+        sec = str(int((num%3600)%60))
+        self.count_hr =  hr.zfill(2)
+        self.count_min = min.zfill(2)
+        self.count_sec = sec.zfill(2)
+        self.count = f'{hr.zfill(2)}:{min.zfill(2)}:{sec.zfill(2)}'
+        
+
+class TimerApp(App):
+    def __init__(self,**kwargs):
+        super(TimerApp,self).__init__(**kwargs)
+        self.title = 'Timer window'
+
+if  __name__=='__main__':
+    TimerApp().run()
+
+
+
